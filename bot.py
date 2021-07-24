37e285a8435aa24054cee1caab0717af2027b353
Lim Yu Long <yulonglimbleh@gmail.com>
2021-07-24 10:23:26 +0800

    bug fixes and extra info for the user

--- bot.py
+++ bot.py
@@ -31,11 +31,12 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 # /start command
 def start(update, context):
-    name = update.message.from_user.first_name;
+    name = update.message.from_user.first_name
     context.bot.send_message(chat_id=update.effective_chat.id, text = """
 Hello {}!
 This bot allows you to eat with your fwiends UwU
 /startsession in a group to start FoodSanta-ing.
+For the bot to work, private message the bot /start.
 """.format(name))
 
 
@@ -59,12 +60,12 @@ def startSession(update, context):
             [InlineKeyboardButton("Start", callback_data="Start")]
             ]
         reply_markup=InlineKeyboardMarkup(reply_keyboard)
-        sessionMessage = context.bot.sendMessage(chatID, 'Hey yall, a FOOD SANTA 🎅 session has been created! Click join to participate in this session.', reply_markup = reply_markup)
+        sessionMessage = context.bot.sendMessage(chatID, 'Hey yall, a FOOD SANTA 🎅 session has been created! Click join to participate in this session. For the bot to work, private message the bot /start.', reply_markup = reply_markup)
 
     else:
         # Session already exists in group
         context.bot.send_message(chatID, text = """
-dude wtf a session alr exists.""")
+ A session alr exists in this chat.""")
         
 
 def closeSession(update, context) -> None:
@@ -126,7 +127,7 @@ def button(update: Update, context: CallbackContext) -> None:
             context.bot.sendMessage(chatID, 'There are not enough participants to start a session :(')
         elif session.startSession():
             context.bot.sendMessage(chatID, 'Session started! Check your DMs! Meanwhile...')
-            setPriceRange(update, context);
+            setPriceRange(update, context)
         else:
             context.bot.sendMessage(chatID, 'Session already started. Check your DMs.')
 
@@ -178,34 +179,40 @@ def receivePollAnswer(update: Update, context: CallbackContext) -> None:
 
 
 def getDetail(update, context):
-    sess = None
-    address = update.message.text.split(" ")
-    address2 = address[1:]
-    address3 = ''
-    for i in address2:
-        address3 += i + " "
-    currentUser = update.effective_user.id
-    allsess = getSessions()
-    for sess in allsess:
-        if currentUser in getSession(sess).userList.keys():
-            getSession(sess).userList[currentUser].setAddress(str(address3))
-            break
 
-    if (sess == None):
-        return 
+    chatID = update.effective_chat.id
+    if not chatID > 0:
+        # User sent this message in a group
+        context.bot.send_message(chatID, text = "This command should be used in private message")
+    else:
+        sess = None
+        address = update.message.text.split(" ")
+        address2 = address[1:]
+        address3 = ''
+        for i in address2:
+            address3 += i + " "
+        currentUser = update.effective_user.id
+        allsess = getSessions()
+        for sess in allsess:
+            if currentUser in getSession(sess).userList.keys():
+                getSession(sess).userList[currentUser].setAddress(str(address3))
+                break
 
-    finished = True
-    for user in getSession(sess).userList.values():
-        if user.address == "":
-            finished = False
-        else:
-            finished = finished & True
+        if (sess == None):
+            return
 
-    update.message.reply_text("You've sent " + address3 + ". Please wait for the rest to complete their entry!")
-    if finished == True:
+        finished = True
         for user in getSession(sess).userList.values():
-            Session.messageUser(getSession(sess), user.userId, "Hello! you'll be sending your goods to "+ user.assigned.username + ", here are the details: " + user.assigned.address + ". Have Fun!!!!")
+            if user.address == "":
+                finished = False
+            else:
+                finished = finished & True
 
+        update.message.reply_text("You've sent " + address3 + ". Please wait for the rest to complete their entry!")
+        if finished == True:
+            for user in getSession(sess).userList.values():
+                Session.messageUser(getSession(sess), user.userId, "Hello! you'll be sending your goods to "+ user.assigned.username + ", here are the details: " + user.assigned.address + ". Have Fun!!!!")
+            deleteSession(sess)
 
 # def test(update, context):
 #     chatID = update.effective_chat.id
